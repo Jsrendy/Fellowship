c1096623eda4d853e213fcc9676c1237eac5d3f5
Jsrendy <77850245+Jsrendy@users.noreply.github.com>
2021-02-21 08:16:17 +0700

    MLH Fellowship
    url

---
+++ coderdy.py
@@ -0,0 +1,6 @@
+grade = int(input("Berapa nilai murid baba st //stundent ? ,,inputh: 't -sntundent" ))
+if grade >=85:print("Grade A")
+elif grade >=70:print("Grade B")
+elif grade >=60:print("Grade C")
+elif grade >=50:print("Grade D")
+else:print(" 0 Grade Tidak LULUS , under graduante ")
